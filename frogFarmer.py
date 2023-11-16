5535a43b11c20bb9f9dd4ec800c399ad738fc6b2
NeoPlays <80448387+NeoPlays@users.noreply.github.com>
2023-11-16 13:54:04 +0300

    add script

---
+++ frogFarmer.py
@@ -0,0 +1,29 @@
+import pyautogui
+import sys
+import time
+
+heightOffset = float(sys.argv[1])
+
+
+screenWidth, screenHeight = pyautogui.size()
+currentMouseX, currentMouseY = pyautogui.position()
+
+print(screenWidth, screenHeight)
+print(currentMouseX, currentMouseY)
+time.sleep(15)
+
+
+
+while True:
+    pyautogui.moveTo(screenWidth/2, (screenHeight/2) + heightOffset)
+    pyautogui.click()
+    time.sleep(15)
+    pyautogui.move(0, 45)
+    pyautogui.click()
+    time.sleep(15)
+    pyautogui.move(0, 55)
+    pyautogui.click()
+    time.sleep(15)
+    pyautogui.move(0, 45)
+    pyautogui.click()
+    time.sleep(15)
